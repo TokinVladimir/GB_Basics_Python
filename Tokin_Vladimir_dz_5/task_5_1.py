19e648c06c7e69f04607df46ec26b74ab63eed1b
Tokin <ress72@mail.ru>
2022-02-16 14:31:17 +1000

    dz5 complete

---
+++ Tokin_Vladimir_dz_5/task_5_1.py
@@ -0,0 +1,31 @@
+'''
+Задание 1. Написать генератор нечётных чисел от 1 до n (включительно), используя ключевое слово yield, например:
+
+$ odd_to_15 = odd_nums(15)
+$ next(odd_to_15)
+1
+$ next(odd_to_15)
+3
+...
+$ next(odd_to_15)
+15
+$ next(odd_to_15)
+...StopIteration...
+'''
+
+from xml.dom.pulldom import parseString
+
+def odd_nums(number: int) -> int:
+    """Генератор, возвращающий по очереди нечетные целые числа от 1 до number (включительно)"""
+    for num in range(1, number + 1, 2):
+        yield num
+
+number = 38
+num = odd_nums(number)
+print(*num)
+
+# n = 25
+# generator = odd_nums(n)
+# for _ in range(1, n+1, 2):
+#     print(next(generator))
+#next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
