9355b75fb997d3e46ba32a0e6cc3be469bb33075
Tokin <ress72@mail.ru>
2022-02-16 14:33:40 +1000

    dz5 complete

--- Tokin_Vladimir_dz_5/task_5_1.py
+++ Tokin_Vladimir_dz_5/task_5_1.py
@@ -13,8 +13,6 @@ $ next(odd_to_15)
 ...StopIteration...
 '''
 
-from xml.dom.pulldom import parseString
-
 def odd_nums(number: int) -> int:
     """Генератор, возвращающий по очереди нечетные целые числа от 1 до number (включительно)"""
     for num in range(1, number + 1, 2):
